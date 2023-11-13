5992ef1d74fe9c1485a75f1e6b63caba7d471b10
alisoneoz <alisonestephanyoz@gmail.com>
2023-11-13 10:15:05 -0400

    Implicit types

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
 console.log('hello')
+
+// Implicit Types
+
+let helloWorld = "Hello, World" // by hovering on top of the variable name, you'll see that TS already assigned the "string" type to this variable, based on its value
+
+helloWorld = 5 //❌ in TS you cannot assign a different type to a variable because in practice it may lead to unexpected behaviour on your code
