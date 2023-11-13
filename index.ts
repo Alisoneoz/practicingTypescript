4a23086b8f9896112079c85d7c3686025dea58ad
alisoneoz <alisonestephanyoz@gmail.com>
2023-11-13 10:18:37 -0400

    declared variables without value will have a type of any

--- index.ts
+++ index.ts
@@ -5,3 +5,9 @@ console.log('hello')
 let helloWorld = "Hello, World" // by hovering on top of the variable name, you'll see that TS already assigned the "string" type to this variable, based on its value
 
 helloWorld = 5 //❌ in TS you cannot assign a different type to a variable because in practice it may lead to unexpected behaviour on your code
+
+let ble; //variables without value have a type of "any"
+
+ble = 0;
+
+ble = "2" // and as it, it allows to mix the type... watchout...
