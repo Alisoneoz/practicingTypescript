4b61072176877c537f661f21ee111584bcae918c
alisoneoz <alisonestephanyoz@gmail.com>
2023-11-17 10:42:19 -0400

    Explicit types

--- index.ts
+++ index.ts
@@ -11,3 +11,19 @@ let ble; //variables without value have a type of "any"
 ble = 0;
 
 ble = "2" // and as it, it allows to mix the type... watchout...
+
+
+// Explicit types
+
+let firstName: string = 'Estephany'; // to declare type, before the '=' add a colon ':' and then the type
+
+let age: number = 111;
+
+let blablito: string; // you can also do it for variables declared with empty value
+
+blablito = 'hello, michi';
+
+blablito = 5 // it will not allow you to assign different types to the variable
+
+
+// IN CONCLUSION --> to specify te type --> add a colon and specify the type
